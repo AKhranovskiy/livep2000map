35959ebda5cbd80805801f4ec9ce337c0cedc059
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2020-02-07 19:24:45 +0100

    Store new items

--- index.js
+++ index.js
@@ -1,60 +1,89 @@
+const https = require('https');
 const EventEmitter = require('events');
+const Parser = require('rss-parser');
+
 class FeedUpdateEmitter extends EventEmitter {}
 
-let Parser = require('rss-parser');
-let parser = new Parser();
 
-const https = require('https');
-const options = {
-  hostname: 'feeds.livep2000.nl',
-  path: '/',
-  method: 'HEAD'
-};
+const FEED_URL = 'https://feeds.livep2000.nl/';
 
 var lastETag = ""
 
 const feedUpdatesEmitter = new FeedUpdateEmitter()
 feedUpdatesEmitter.on('check', () => {
-  emitter = this
-  https.request(options, (res) => {
-    etag = res.headers.etag
-    if (lastETag != etag) {
-      console.log('has updates, %s != %s', lastETag, etag)
-      lastETag = etag
-      feedUpdatesEmitter.emit('hasUpdate')
-    } else {
-      console.log('No updates')
-    }
-  }).on('error', (e) => {
-    console.error(e);
-  }).end();
+    https.request(FEED_URL, {
+        method: "HEAD"
+    }, (res) => {
+        etag = res.headers.etag
+        if (lastETag != etag) {
+            console.log('has updates, %s != %s', lastETag, etag)
+            lastETag = etag
+            feedUpdatesEmitter.emit('hasUpdate')
+        } else {
+            console.log('No updates')
+        }
+    }).on('error', (e) => {
+        console.error(e);
+    }).end();
 })
 
 feedUpdatesEmitter.on('hasUpdate', () => {
-  console.log('Downloading feed')
-  https.get('https://feeds.livep2000.nl/', (res) => {
-    etag = res.headers.etag
-    lastETag = etag
-    console.log('Downloaded %d bytes', parseInt(res.headers['content-length'], 10))
-    var rawData = '';
-    res.on('data', (chunk) => { rawData += chunk})
-    res.on('end', () => {
-      setImmediate((data) => feedUpdatesEmitter.emit('update', rawData), rawData)
-    });
-}).on('error', (e) => {
-  console.error(e);
-}).end();
+    console.log('Downloading feed')
+    https.request(FEED_URL, {
+        method: "GET"
+    }, (res) => {
+        etag = res.headers.etag
+        lastETag = etag
+        console.log('Downloaded %d bytes', parseInt(res.headers['content-length'], 10))
+        var rawData = '';
+        res.on('data', (chunk) => {
+            rawData += chunk
+        })
+        res.on('end', () => {
+            setImmediate((data) => feedUpdatesEmitter.emit('update', rawData), rawData)
+        });
+    }).on('error', (e) => {
+        console.error(e);
+    }).end();
 })
 
-feedUpdatesEmitter.on('update', (data) => {
-  //console.log('All data received, %d bytes', data.length)
-
-  (async () => {
-    let feed = await parser.parseString(data)
-    console.log(feed.title)
-    console.log(feed.items.length)
-  })();
+feedUpdatesEmitter.on('update', async (data) => {
+    let feed = await new Parser({
+        customFields: {
+            item: [
+                ['geo:lat', 'latitude'],
+                ['geo:long', 'longitude']
+            ]
+        }
+    }).parseString(data)
+    feed.items.reverse().forEach(item => {
+        process.nextTick(item => feedUpdatesEmitter.emit('item', item), item)
+    })
 });
 
 feedUpdatesEmitter.emit('hasUpdate')
 checkTimer = setInterval(() => feedUpdatesEmitter.emit('check'), 5 * 1000)
+
+process.on('SIGINT', (sig) => {
+    setImmediate(() => clearInterval(checkTimer))
+})
+
+process.on('exit', (code) => {
+    console.log('Process exit event with code: ', code);
+});
+
+var items = new Map()
+
+feedUpdatesEmitter.on('item', item => {
+    if (!items.has(item.guid)) {
+        items.set(item.guid, item)
+        console.log('Received new item %s, total %d', item.guid, items.size)
+        process.nextTick(() => feedUpdatesEmitter.emit('eventAdded', item.guid))
+    }
+})
+
+feedUpdatesEmitter.on('eventAdded', guid => {
+    ev = items.get(guid)
+    console.log('New event recoreded %s on %s at %s:%s',
+        guid, ev.pubDate, ev.latitude || 'X', ev.longitude || 'Y')
+})

e867f16f8b52a928004f31527d6312b5e52bb513
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2020-02-16 17:22:43 +0100

    Query feed etag
    Does not work properly without promise

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
-const functions = require('firebase-functions');
+'use strict'
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const https = require('https');
+const functions = require('firebase-functions');
+const parser = require('rss-parser');
+const util = require('util')
+
+const FEED_URL = 'https://feeds.livep2000.nl/';
+
+exports.checkrssfeed = functions.https.onRequest((request, response) => {
+    return https.request(FEED_URL, {
+        method: "HEAD"
+    }, (res) => {
+        const etag = res.headers.etag
+        console.log('Feed ETag %s', etag)
+        response.status(200).send(util.format('ETag %s', etag))
+    }).on('error', (e) => {
+        console.error(e);
+        response.status(200).send("Failed to get etag")
+    }).end();
+});

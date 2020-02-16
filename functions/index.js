889369ccfce2f6cf16e1f67a81246093018e21eb
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2020-02-16 17:40:21 +0100

    Request with Promise

--- functions/index.js
+++ functions/index.js
@@ -8,14 +8,18 @@ const util = require('util')
 const FEED_URL = 'https://feeds.livep2000.nl/';
 
 exports.checkrssfeed = functions.https.onRequest((request, response) => {
-    return https.request(FEED_URL, {
+  return  new Promise((resolve, reject) => {
+    https.request(FEED_URL, {
         method: "HEAD"
     }, (res) => {
         const etag = res.headers.etag
         console.log('Feed ETag %s', etag)
         response.status(200).send(util.format('ETag %s', etag))
+        res.on('end', () => resolve())
     }).on('error', (e) => {
         console.error(e);
         response.status(200).send("Failed to get etag")
+        res.on('end', () => resolve())
     }).end();
+  })
 });
